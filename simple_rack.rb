9a6bda5c42d4e826ebfb4ca3016e6407740f8086
Mwaki Magotswi <magotswi@gmail.com>
2013-01-13 23:05:38 -0500

    refactoring simple rack, inspired by psullivan's input on day 6 tech forum

--- simple_rack.rb
+++ simple_rack.rb
@@ -1,2 +1,2 @@
-simple_rack = lambda { |env| [200, {"Content-Type" => "text/plain"}, ["Command line argument you typed was: #{ARGV.first.nil? ? 'No command line input' : ARGV.first}"]] }
-puts simple_rack.call({})
+simple_rack = lambda { |env| [200, {"Content-Type" => "text/plain"}, ["Command line argument you typed was: #{env[:argument]}"]] }
+puts simple_rack.call({:argument => ARGV.first.nil? ? 'No command line input' : ARGV.first})
